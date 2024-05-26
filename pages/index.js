32176f662e95451e840b35c60b80109573c5a35b
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2024-05-26 13:46:33 +0300

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,32 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import Nav from "../components/Nav.jsx"
+import Header from "@/components/Header.jsx";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "../components/Nav.jsx";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if(!session){
+  const { data: session } = useSession();
+  if (!session) {
     return (
-      <div className='bg-blue-900 w-screen h-screen flex items-center'>
-  
-       <div className="w-full text-center">
-        <button onClick={() => signIn('google')} className="bg-white p-2 rounded-lg px-4">Login with Google</button>
-       </div>
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="w-full text-center">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 rounded-lg px-4"
+          >
+            Login with Google
+          </button>
+        </div>
       </div>
-    )
+    );
   }
   return (
-    <div className='bg-neutral-400 min-h-screen'>
-      <Nav/>
-       logged in {session.user.email}
+    <div>
+      <Header />
+      <div className="flex">
+        <div className="bg-neutral-400 min-h-screen min-w-48 ">
+          <Nav />
+        </div>
+        <div className="bg-white flex-grow">logged in {session.user.email}</div>
+      </div>
     </div>
   );
 }
